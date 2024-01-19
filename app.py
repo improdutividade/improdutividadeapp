81d9b21ac18cd589db2ef675c3db10423a1611c4
improdutividade <156726836+improdutividade@users.noreply.github.com>
2024-01-19 11:30:50 -0300

    Update app.py

--- app.py
+++ app.py
@@ -219,15 +219,45 @@ user_id = str(uuid.uuid4())
 registro = RegistroAtividades(user_id)
 analise = AnaliseAtividades(user_id)
 
+def descricao_app1():
+    st.title("App 1 - Registro de Atividades (AtividadeTracker)")
+    st.write(
+        "O AtividadeTracker é uma aplicação projetada para simplificar o registro "
+        "de atividades individuais de membros de uma equipe. Com essa ferramenta intuitiva, "
+        "os usuários podem facilmente monitorar e documentar as atividades realizadas durante um determinado período.\n\n"
+        "Principais recursos do AtividadeTracker:\n"
+        "- Registro Individual: Permite que cada membro da equipe registre suas atividades de maneira personalizada.\n"
+        "- Controle de Tempo: Registra automaticamente o início e o fim de cada atividade, calculando a duração total.\n"
+        "- Relatórios Personalizados: Gera relatórios detalhados e personalizáveis para análise e download.\n\n"
+        "Seja para equipes de construção civil, escritórios ou outros setores, "
+        "o AtividadeTracker simplifica o processo de rastreamento de atividades, "
+        "promovendo uma gestão mais eficiente do tempo e recursos."
+    )
+
+def descricao_app2():
+    st.title("App 2 - Análise de Atividades (ConstruData Insights)")
+    st.write(
+        "O ConstruData Insights é uma aplicação especializada na análise e visualização da distribuição "
+        "de pessoas em diferentes atividades no setor da construção civil. Esta ferramenta foi desenvolvida "
+        "para oferecer insights valiosos sobre a produtividade da equipe e otimizar a alocação de recursos.\n\n"
+        "Principais recursos do ConstruData Insights:\n"
+        "- Análise de Atividades: Permite aos usuários selecionar e quantificar diversas atividades com base em critérios específicos.\n"
+        "- Tempo Real: Oferece informações em tempo real sobre a distribuição de equipe em diferentes tarefas.\n"
+        "- Relatórios Gráficos: Apresenta gráficos e visualizações para facilitar a interpretação dos dados coletados.\n\n"
+        "Seja para gestores de projeto, supervisores ou tomadores de decisão, "
+        "o ConstruData Insights é uma ferramenta valiosa para melhorar a eficiência operacional e a produtividade "
+        "na indústria da construção civil."
+    )
+
 def informacoes():
     st.title("Informações")
     st.write("Bem-vindo à página de informações. Aqui você encontrará detalhes sobre os aplicativos.")
 
     st.header("Sobre o App 1:")
-    st.write("O App 1 é projetado para registrar atividades individuais de membros da equipe.")
+    descricao_app1()
 
     st.header("Sobre o App 2:")
-    st.write("O App 2 é destinado a analisar a distribuição de pessoas em diferentes atividades no setor da construção civil.")
+    descricao_app2()
 
 # Gráficos
 def graficos():
