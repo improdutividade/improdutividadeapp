6130f77944d7c46101844d813440a79e6ee6c336
improdutividade <156726836+improdutividade@users.noreply.github.com>
2024-01-21 01:25:17 -0300

    Update app.py

--- app.py
+++ app.py
@@ -211,7 +211,6 @@ def get_binary_file_downloader_html(bin_file, file_label='File'):
     href = f'<a href="data:application/octet-stream;base64,{bin_str}" download="{os.path.basename(bin_file)}">{file_label}</a>'
     return href
 
-# Adicionado um identificador único para cada usuário usando o UUID
 user_id = str(uuid.uuid4())
 registro = RegistroAtividades(user_id)
 analise = AnaliseAtividades(user_id)
